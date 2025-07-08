991d13f58bc511d30d2767a8d54efed0860c8602
nawseekhiya <araj80317@gmail.com>
2025-07-08 23:30:48 +0530

    feat(client): setup App routing and base layout structure

---
+++ client/src/pages/SourcesPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SourcesPage = () => {
+  return (
+    <div>
+      Sources Page (WIP)
+    </div>
+  )
+}
+
+export default SourcesPage
---
+++ client/src/pages/SuccessPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SuccessPage = () => {
+  return (
+    <div>
+      Success Page (WIP)
+    </div>
+  )
+}
+
+export default SuccessPage
